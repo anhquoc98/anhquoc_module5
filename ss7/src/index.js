b43dff54bf2e30b02265e251a1e6321fcac1d418
anhquoc98 <anhquoc98dn@gmail.com>
2023-05-09 20:36:04 +0700

    up

--- ss7/src/index.js
+++ ss7/src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as PropTypes from "prop-types";
-import * as root from "react-dom";
 
-
-
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
-          <App />
-
-
+    <App />
   </React.StrictMode>
 );
 
